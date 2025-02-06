0a1c33240e8417eb1fcee1e38288941bfbb41aeb
GARajab <mohamed.rajab@ewa.bh>
2025-02-06 17:04:21 +0300

    notify be bootstrap is done but want to change it

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -14,8 +14,25 @@ def about(request):
 
 
 def login_user(request):
-    return render(request, "login.html", {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You Are Logged In Successfully"))
+            return redirect("home")
+        else:
+            messages.success(
+                request, ("There Was An Error Call Mohamed Rajab On 17991236")
+            )
+            return redirect("login")
+
+    else:
+        return render(request, "login.html", {})
 
 
 def logout_user(request):
-    return render(request, "logout.html", {})
+    logout(request)
+    messages.success(request, ("You Are LogedOut Successfully!"))
+    return redirect("home")

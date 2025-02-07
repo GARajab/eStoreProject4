3ef7e9a9afb168180218de7eaeda40d37eb16018
GARajab <mohamed.rajab@ewa.bh>
2025-02-07 16:56:14 +0300

    start with cart, product page done,register new user done

--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Category, Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 
 def home(request):
@@ -23,9 +27,7 @@ def login_user(request):
             messages.success(request, ("You Are Logged In Successfully"))
             return redirect("home")
         else:
-            messages.success(
-                request, ("There Was An Error Call Mohamed Rajab On 17991236")
-            )
+            messages.warning(request, ("There Was An Error Try Again Later"))
             return redirect("login")
 
     else:
@@ -34,5 +36,56 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    messages.success(request, ("You Are LogedOut Successfully!"))
+    messages.info(request, ("You Are LogedOut Successfully!"))
     return redirect("home")
+
+
+def category(request, rjb):
+    rjb = rjb.replace("-", "")
+    try:
+        category = Category.objects.get(name=rjb)
+        products = Product.objects.filter(category=category)
+
+        # Check if there are no products in the category
+        if not products.exists():
+            messages.info(
+                request,
+                f"There are no products available in the category '{category.name}'.",
+            )
+            return redirect("home")
+
+        return render(
+            request, "category.html", {"products": products, "category": category}
+        )
+
+    except Category.DoesNotExist:
+        messages.error(request, ("This Category Is Not Available"))
+        return redirect("home")
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, "product.html", {"product": product})
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # add celepration
+            messages.success(request, ("You Have Registered Successfully!!"))
+            return redirect("home")
+        else:
+            messages.success(
+                request, ("Sorry There Was Something Wrong, Please Try Again!!")
+            )
+            return redirect("register")
+
+    return render(request, "register.html", {"form": form})
